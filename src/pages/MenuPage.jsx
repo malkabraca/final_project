0dfd416786eaca4eb40fa5bb38c831c540cbebcc
malkabraca <m0502800840@gmail.com>
2023-07-03 11:47:27 +0300

    meno hom

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
 import {
-Button,
+  Button,
   Card,
   Col,
   Container,
@@ -8,66 +7,137 @@ Button,
   Image,
   ListGroup,
   Row,
+  Spinner,
 } from "react-bootstrap";
 import { BsCurrencyDollar } from "react-icons/bs";
-import ImagePopup from "../components/ImagePopup";
 import CardMenu from "../components/CardMenu";
+import { toast } from "react-toastify";
+import useQueryParams from "../hooks/useQueryParams";
+import { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const MenuPage = () => {
-  /* const ImagePopup = ({ imageUrl }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [originalCardsArr, setOriginalCardsArr] = useState(null);
+  const [cardsArr, setCardsArr] = useState(null);
+  const navigate = useNavigate();
+  let qparams = useQueryParams();
+  const payload = useSelector((bigPie) => bigPie.authSlice.payload);
 
-  const openPopup = () => {
-    setIsOpen(true);
+  useEffect(() => {
+    axios
+      .get("/cards")
+      .then(({ data }) => {
+        filterFunc(data);
+      })
+      .catch((err) => {
+        toast.error("err from axios" + "" + err.response.data);
+      });
+  }, []);
+
+  const filterFunc = (data) => {
+    if (!originalCardsArr && !data) {
+      return;
+    }
+    let filter = "";
+    if (qparams.filter) {
+      filter = qparams.filter;
+    }
+    if (!originalCardsArr && data) {
+      /*
+        when component loaded and states not loaded
+      */
+      setOriginalCardsArr(data);
+      setCardsArr(
+        data.filter(
+          (card) =>
+            card.title.startsWith(filter) || card.bizNumber.startsWith(filter)
+        )
+      );
+      return;
+    }
+    if (originalCardsArr) {
+      /*
+        when all loaded and states loaded
+      */
+      let newOriginalCardsArr = JSON.parse(JSON.stringify(originalCardsArr));
+      setCardsArr(
+        newOriginalCardsArr.filter(
+          (card) =>
+            card.title.startsWith(filter) || card.bizNumber.startsWith(filter)
+        )
+      );
+    }
   };
 
-  const closePopup = () => {
-    setIsOpen(false);
-  }} */
- const imageUrls = [
-   "../image/chefs-2.jpg",
-"../image/chefs-2.jpg"
- 
- ];
-  
-    /* const imageUrls = ["../image/chefs-2.jpg", "../image/chefs-2.jpg"]; */
-console.log(imageUrls[1]);
-console.log(imageUrls);
+  useEffect(() => {
+    filterFunc();
+  }, [qparams.filter]);
 
-const card=[ {
-  "title": "card1",
-  "description": "Create a first card",
-  "imageUrl": "https://cdn.xxl.thumbs.canstockphoto.co.il/%D7%99%D7%9C%D7%93%D7%94-%D7%95%D7%A7%D7%98%D7%95%D7%A8-%D7%93%D7%95%D7%92%D7%9E%D7%94-%D7%91%D7%95%D7%91%D7%94-%D7%AA%D7%9E%D7%95%D7%A0%D7%94_csp7498887.jpg",
-  "imageAlt": "doll",
-  "price": "150"
-},
-{
-  "title": "card2",
-  "description": "Create a first card",
-  "imageUrl": "https://cdn.xxl.thumbs.canstockphoto.co.il/%D7%99%D7%9C%D7%93%D7%94-%D7%95%D7%A7%D7%98%D7%95%D7%A8-%D7%93%D7%95%D7%92%D7%9E%D7%94-%D7%91%D7%95%D7%91%D7%94-%D7%AA%D7%9E%D7%95%D7%A0%D7%94_csp7498887.jpg",
-  "imageAlt": "doll",
-  "price": "100"
-},
-{
-  "title": "card3",
-  "description": "Create a first card",
-  "imageUrl": "https://cdn.xxl.thumbs.canstockphoto.co.il/%D7%99%D7%9C%D7%93%D7%94-%D7%95%D7%A7%D7%98%D7%95%D7%A8-%D7%93%D7%95%D7%92%D7%9E%D7%94-%D7%91%D7%95%D7%91%D7%94-%D7%AA%D7%9E%D7%95%D7%A0%D7%94_csp7498887.jpg",
-  "imageAlt": "doll",
-  "price": "50"
-}]
+  const handleDeleteFromInitialCardsArr = async (id) => {
+    try {
+      await axios.delete("/cards/" + id);
+      setCardsArr((newCardsArr) =>
+        newCardsArr.filter((item) => item._id != id)
+      );
+    } catch (err) {
+      toast.error(err.response.data);
+    }
+  };
+  const handleEditFromInitialCardsArr = (id) => {
+    const cardToEdit = cardsArr.find((card) => card._id == id);
+    navigate(`/edit/${id}`, { state: { user_id: cardToEdit.user_id } });
+  };
+  const handleMoreInformationFromInitialCardsArr = (id) => {
+    navigate(`/infor/${id}`);
+  };
+  if (!cardsArr) {
+    return  <Spinner animation="border" role="status"></Spinner>
+  }
+  const deleteHome = () => {};
+
+  // const card = [
+  //   {
+  //     title: "card1",
+  //     description: "Create a first card",
+  //     imageUrl:
+  //       "https://cdn.xxl.thumbs.canstockphoto.co.il/%D7%99%D7%9C%D7%93%D7%94-%D7%95%D7%A7%D7%98%D7%95%D7%A8-%D7%93%D7%95%D7%92%D7%9E%D7%94-%D7%91%D7%95%D7%91%D7%94-%D7%AA%D7%9E%D7%95%D7%A0%D7%94_csp7498887.jpg",
+  //     imageAlt: "doll",
+  //     price: "150",
+  //   },
+  //   {
+  //     title: "card2",
+  //     description: "Create a first card",
+  //     imageUrl:
+  //       "https://cdn.xxl.thumbs.canstockphoto.co.il/%D7%99%D7%9C%D7%93%D7%94-%D7%95%D7%A7%D7%98%D7%95%D7%A8-%D7%93%D7%95%D7%92%D7%9E%D7%94-%D7%91%D7%95%D7%91%D7%94-%D7%AA%D7%9E%D7%95%D7%A0%D7%94_csp7498887.jpg",
+  //     imageAlt: "doll",
+  //     price: "100",
+  //   },
+  //   {
+  //     title: "card3",
+  //     description: "Create a first card",
+  //     imageUrl:
+  //       "https://cdn.xxl.thumbs.canstockphoto.co.il/%D7%99%D7%9C%D7%93%D7%94-%D7%95%D7%A7%D7%98%D7%95%D7%A8-%D7%93%D7%95%D7%92%D7%9E%D7%94-%D7%91%D7%95%D7%91%D7%94-%D7%AA%D7%9E%D7%95%D7%A0%D7%94_csp7498887.jpg",
+  //     imageAlt: "doll",
+  //     price: "50",
+  //   },
+  // ];
   return (
     <Container>
       <h1 className="title"> menu</h1>
       <Row className="mb-3">
-      {card.map((item) => (
-              <CardMenu
-              key={item.title}
-              imageUrl = {item.imageUrl}
-              title={item.title}
-              description={item.description}
-              price={item.price}
-              />
-            ))}
+        {cardsArr.map((item) => (
+          <CardMenu
+            key={item._id + Date.now()}
+            imageUrl={item.imageUrl}
+            imageAlt ={item.imageAlt}
+            title={item.title}
+            description={item.description}
+            price={item.price}
+          />
+        ))}
       </Row>
     </Container>
   );
